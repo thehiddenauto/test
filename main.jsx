5fd0ad60001172e052e01bed10e3b9be3eeae420
thehiddenauto <thehiddenauto@gmail.com>
2025-08-14 00:53:08 +0200

    Update main.jsx

--- main.jsx
+++ main.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import RootApp from './App.jsx'
+import App from './App.jsx'
 import './index.css'
 
+// Initialize React app
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RootApp />
+    <App />
   </React.StrictMode>,
 )
